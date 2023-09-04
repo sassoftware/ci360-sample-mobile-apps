2a681c57b5ec58440e07985ccf41f46eb944d0d6
Ryan Lee <ryan.lee@sas.com>
2023-09-04 06:11:50 +0000

    Upload New File

---
+++ newPageExample..tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { newPage } from 'mobile-sdk-react-native';
+
+const newPageExample: React.FC = () => {
+	return (
+		newPage('outdoor/fishing/livebait');
+	);
+};
