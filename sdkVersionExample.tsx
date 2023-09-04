616223df09c7951e5ed69f24d37cf0b0d6f1b88b
Ryan Lee <ryan.lee@sas.com>
2023-09-04 06:09:00 +0000

    Example for sdkVersion

---
+++ sdkVersionExample.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Platform } from 'react-native';
+import { getSdkVersion } from 'mobile-sdk-react-native';
+
+const sdkVersionExample: React.FC = () => {
+	const [sdkVersion, setSdkVersion] = useState<string>('');
+	useEffect(() => {
+	    getSdkVersion((version: string) => {
+	        setSdkVersion(version);
+	    });
+	}, []);
+	return (
+	 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+	     <Text>{Platform.OS} SDK version: {sdkVersion}</Text>
+	 </View>
+	);
+};
