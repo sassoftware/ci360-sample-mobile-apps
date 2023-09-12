8dc3ed49c107355486ed0bd882e3545c96bed40f
Ryan Lee <ryan.lee@sas.com>
2023-09-12 03:59:33 +0000

    Replace identityExample.tsx

--- identityExample.tsx
+++ identityExample.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, TextInput } from 'react-native';
 import { identity } from 'mobile-sdk-react-native';
 
 
-const sdkVersionExample: React.FC = () => {
-	const [loginType, setLoginType] = React.useState<string>('');
+const identityExample: React.FC = () => {
 	const [userId, setUserId] = React.useState<string>('');
+	const loginType = 'IDENTITY_TYPE_CUSTOMER_ID';
+	// const loginType = 'IDENTITY_TYPE_LOGIN_ID';
 
+	const handlePress = async () => {
+		try {
+			await identity(userId, loginType);
+			console.log('Log-in Success');
+		} catch (error) {
+			console.log('Log-in Failure');
+		}
+	};
+	   
 	return (
 	 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+		<TextInput
+			placeholder="Enter User ID"
+			onChangeText={setUserId} // Capture and update the userId state
+		/>
 	   <Button title="Identity" onPress={handlePress} />
 	 </View>
 	);
 };
+
+export default identityExample;
