492fbeb8c331ad02aa7d2842e55920d806c39103
Ryan Lee <ryan.lee@sas.com>
2023-09-12 03:59:42 +0000

    Replace newPageExample.tsx

--- newPageExample.tsx
+++ newPageExample.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Platform } from 'react-native';
 import { newPage } from 'mobile-sdk-react-native';
 
 const newPageExample: React.FC = () => {
-	return (
+	useEffect(() => {
 		newPage('outdoor/fishing/livebait');
+	}, []);
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>{Platform.OS} newPage event sent.</Text>
+		</View>
 	);
 };
+
+export default newPageExample;
