905adffccd013ca95d4cc167670f5b03ec1eae4d
Ryan Lee <ryan.lee@sas.com>
2023-09-12 03:59:53 +0000

    Replace sdkVersionExample.tsx

--- sdkVersionExample.tsx
+++ sdkVersionExample.tsx
@@ -4,14 +4,18 @@ import { getSdkVersion } from 'mobile-sdk-react-native';
 
 const sdkVersionExample: React.FC = () => {
 	const [sdkVersion, setSdkVersion] = useState<string>('');
+
 	useEffect(() => {
 	    getSdkVersion((version: string) => {
 	        setSdkVersion(version);
 	    });
 	}, []);
+
 	return (
 	 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 	     <Text>{Platform.OS} SDK version: {sdkVersion}</Text>
 	 </View>
 	);
 };
+
+export default sdkVersionExample;
